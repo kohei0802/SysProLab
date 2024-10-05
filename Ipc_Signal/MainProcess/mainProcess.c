5afa82e86db4f7c5fb256fc085f4ab891d22577a
kohei0802 <koheileung0208@gmail.com>
2024-10-05 16:30:32 +0800

    Added feature: Dispatch thread to retrieve /proc info periodically

--- Ipc_Signal/MainProcess/mainProcess.c
+++ Ipc_Signal/MainProcess/mainProcess.c
@@ -5,23 +5,79 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
+
+#include <stdbool.h>
 
 
-#define msg_t 257
+#define MSG_SIZE 257
 
-pid_t childPid;
+#define MON_INT 300
+
+pid_t childPid=0;
+bool inferencing=false; //can be changed to semaphore for efficiency
 
 void sigintHandler(int signum);
 void sigusr1Handler(int signum);
 
 void clearStdin();
 
+void *threadMonitor(void *arg)
+{
+    const int INPUTSIZE = 50;
+    char output[INPUTSIZE*10];
+    FILE* procFile = NULL;
+    char procPath[INPUTSIZE];
+    char procText[INPUTSIZE];
+
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+    snprintf(procPath, sizeof(procPath), "/proc/%d/stat", childPid);
+    
+    /*(1)pid (2)comm (3)state (14)utime (15)stime (19)nice (23)vsize (39)processor (41)policy
+    */
+    const char format[]="%s %s %s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %s %s %*s %*s %*s %s %*s %*s %*s %s \
+                            %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %s %*s %s";
+    char pid[INPUTSIZE], comm[INPUTSIZE], state[INPUTSIZE], utime[INPUTSIZE], stime[INPUTSIZE]
+                , nice[INPUTSIZE], vsize[INPUTSIZE], processor[INPUTSIZE], policy[INPUTSIZE];
+
+    procFile=fopen(procPath, "r");
+    fscanf(procFile, format, pid, comm, state, utime, stime, nice, vsize, processor, policy);
+    fclose(procFile);
+
+    float initialTimeUsage = (float) atoi(utime) + (float) atoi(stime);
+    float initialTime = 
+    usleep(1000 * MON_INT);
+
+    while(true) 
+    {
+        procFile=fopen(procPath, "r");
+        fscanf(procFile, format, pid, comm, state, utime, stime, nice, vsize, processor, policy);
+        
+        //Calculate cpu usage in %
+        float currentUsage = (float)(atoi(utime)+atoi(stime));
+        float cpuusage = (currentUsage-initialTimeUsage) / 30 * 100;
+        initialTimeUsage = currentUsage;
+    
+        snprintf(output, sizeof(output), "%s %s %s %s %s %s %s %s %s %.2f%%", 
+                    pid, comm, state, policy, nice, vsize, processor, utime, stime, (cpuusage>100.0)?100.0:cpuusage);
+        fwrite(output, sizeof(char), strlen(output), stderr);
+        fwrite("\n", sizeof(char), 1, stderr);
+        fflush(stderr);
+        fclose(procFile);
+
+        usleep(1000 * MON_INT);
+        
+        pthread_testcancel();
+    }
+    
+}
 
 int main(int argc, char const *argv[])
 {
     pid_t pid1;
     int requestPipe[2];
-    char inputBuffer[msg_t];
+    char inputBuffer[MSG_SIZE];
 
     pipe(requestPipe);
     pid1 = fork();
@@ -38,32 +94,42 @@ int main(int argc, char const *argv[])
     {
         sigset_t set;
         siginfo_t sigInformation;
+        pthread_t monitorThread;
+
+        childPid = pid1;
+
+        signal(SIGINT, sigintHandler);
+        signal(SIGUSR1, sigusr1Handler);
 
         sigemptyset(&set);
         sigaddset(&set, SIGUSR1);
 
-        childPid = pid1;
-
-        //Handle signal to kill child
-        signal(SIGINT, sigintHandler);
-        signal(SIGUSR1, sigusr1Handler);
-
         printf("%d\n", getpid());
-        while(1) 
+        while(true) 
         {
             printf(">>> ");
             if (!fgets(inputBuffer, sizeof(inputBuffer), stdin))
                 break;
-            // if (!(stdin->_IO_read_ptr == stdin->_IO_read_end))
-            //     clearStdin();
 
-            write(requestPipe[1], inputBuffer, msg_t);
+            if (pthread_create(&monitorThread, NULL, threadMonitor, NULL)) 
+            {
+                perror("Error in creating pthread\n");
+                exit(1);
+            }
+
+            write(requestPipe[1], inputBuffer, MSG_SIZE);
 
             do
             {
                 sigwaitinfo(&set, &sigInformation);
             } while (sigInformation.si_pid != childPid);
 
+            pthread_cancel(monitorThread);
+            pthread_join(monitorThread, NULL);
+            char endMessage[] = "Inference Completed\n";
+            fwrite(endMessage, sizeof(char), strlen(endMessage), stderr);
+
+            //Last statement in this loop
             if (!(stdin->_IO_read_ptr == stdin->_IO_read_end))
                 clearStdin();
             
--- Ipc_Signal/ReceiverProcess/receiverProcess.c
+++ Ipc_Signal/ReceiverProcess/receiverProcess.c
@@ -3,24 +3,63 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <signal.h>
+#include <stdbool.h>
 
-#define in_buffer_t 257
+#define BUFF_SIZE 257
 
 pid_t ppid;
 
 void sigintHandler(int signum);
 
+void swap(int* xp, int* yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n){
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == false)
+            break;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    char inputBuffer[in_buffer_t];
+    char inputBuffer[BUFF_SIZE];
+
+    
 
     signal(SIGINT, sigintHandler);
 
     ppid = getppid();
     while(1)
     {
-        read(STDIN_FILENO, inputBuffer, in_buffer_t);
-        
+        read(STDIN_FILENO, inputBuffer, BUFF_SIZE);
+
+        /*
+            This segment is used to see if the main process can correctly
+            measures the CPU usage of this process
+        */
+        int *arrayTestCPUUsage = malloc(sizeof(int) * 100000);
+        int * p = arrayTestCPUUsage;
+        for(int i=0; i<100000;i++)
+            *(++p)=100000-i;
+        bubbleSort(arrayTestCPUUsage, 100000);
+
         for (int i=0; i<3; i++)
         {
             printf("Received: %s", inputBuffer);
@@ -36,3 +75,4 @@ void sigintHandler(int signum)
 {
     exit(0);
 }
+
