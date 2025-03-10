eecb717b53ea13b1ea3dc17fe23ed15fdd5e7e5f
kohei0802 <koheileung0208@gmail.com>
2025-03-10 03:56:31 -0500

    tcp sending

---
+++ Textconference/src/tools/client.h
@@ -0,0 +1,9 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+/**
+ * @file
+ * 
+ */
+
+#endif // CLIENT_H
---
+++ Textconference/src/tools/server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+/**
+ * @file
+ * 
+ */
+
+
+/**
+ * Read from the socket -> dispatch to sender routine
+ */
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
