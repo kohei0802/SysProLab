6ce9a7e34e53816b320574eaeb7573736a6f10a9
kohei0802 <koheileung0208@gmail.com>
2025-03-12 16:31:51 -0500

    deleted dir

--- Textconference/src/tools/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT_H
-#define CLIENT_H
-
-/**
- * @file
- * 
- */
-
-#endif // CLIENT_H
--- Textconference/src/tools/server.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-/**
- * @file
- * 
- */
-
-#endif // SERVER_H
